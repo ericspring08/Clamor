0a50d7329970379e24156f63ad38894e02d73c32
Eric Zhang <ericspring08@gmail.com>
2022-07-06 11:20:30 -0500

    update

--- src/components/VolumeSlider.js
+++ src/components/VolumeSlider.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Slider from '@mui/material/Slider'
 import Box from '@mui/material/Box'
 import { Typography } from '@mui/material';
